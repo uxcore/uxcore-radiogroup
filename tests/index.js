51b3007566e164e424533381f2e638be61924e9c
知蕾 <wenlei.kwl@alibaba-inc.com>
2018-09-27 14:18:03 +0800

    fix css

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,2 @@
-/**
- * only require other specs here
- */
-
- const req = require.context('.', false, /\.spec\.js$/);
- req.keys().forEach(req);
+const req = require.context(".", false, /\.spec\.js$/);
+req.keys().forEach(req);
